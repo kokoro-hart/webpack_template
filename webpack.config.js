c881a6710e60d5edd1cc3c99b4c41c7d58d96220
kokoro <cocosyu.11261126@gmail.com>
2022-05-08 14:41:21 +0900

    Add: pugを追加

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,10 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const globule = require("globule");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-module.exports = {
+const app = {
   entry: './src/js/main.js',
   output: {
     path: path.resolve(__dirname, './dist'),
@@ -31,6 +32,20 @@ module.exports = {
         generator: {
           filename: 'img/[name][ext]'
         },
+      },
+      {// Pugの設定
+        test: /\.pug$/,
+        use: [
+          {
+            loader: 'html-loader'
+          },
+          {
+            loader: 'pug-html-loader',
+            options: {
+              pretty: true
+            }
+          }
+        ]
       }
     ]
   },
@@ -38,11 +53,34 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename:'./css/style.css',
     }),
-    //htmlのビルド
-    new HtmlWebpackPlugin({
-      filename: 'index.html',
-      template: './src/templates/index.html',
-    }),
+    // //htmlのビルド
+    // new HtmlWebpackPlugin({
+    //   filename: 'index.html',
+    //   template: './src/templates/index.pug',
+    // }),
+    // new HtmlWebpackPlugin({
+    //   filename: 'about.html',
+    //   template: './src/templates/about.pug',
+    // }),
     new CleanWebpackPlugin(),
   ]
 }
+//srcフォルダからpngを探す
+const templates = globule.find("./src/templates/*.pug", {
+  ignore: ["./src/templates/_*.pug"]
+});
+
+//pugファイルがある分だけhtmlに変換する
+templates.forEach((template) => {
+  const fileName = template.replace("./src/templates/", "").replace(".pug", ".html");
+  app.plugins.push(
+    new HtmlWebpackPlugin({
+      filename: `${fileName}`,
+      template: template,
+      //inject: false, //false, head, body, trueから選べる
+      minify: false //本番環境でも圧縮しない
+    }),
+  );
+});
+
+module.exports = app;
